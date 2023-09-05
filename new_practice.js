6e9e5a0b1ac7039c5037ed80baa2ed31c4feb6b6
Edujesseyang <edujesseyang@gmail.com>
2023-09-05 12:56:21 -0700

    a

---
+++ new_practice.js
@@ -0,0 +1,17 @@
+let print = () => {
+    console.log("too easy!");
+    return;
+};
+
+print();
+
+let addUp = (x, y) => {
+    let result;
+    result = x + y;
+    return result;
+};
+
+console.log(addUp(1, 2));
+
+let multiple = (x, y) => (x * y);
+console.log(multiple(1,4));

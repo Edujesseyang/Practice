5ea394ede0bb06a09a04f435a5282359581a3ebf
Edujesseyang <edujesseyang@gmail.com>
2023-09-04 17:09:36 -0700

    super mario bro

--- script.js
+++ script.js
@@ -525,27 +525,216 @@
 // }
 
 //-----------------------------------------------------------------------------
-//另一种制作enent listener 的方法.
-let btn = document.getElementById("button");
-btn.addEventListener("click", function () {
-   
-    btn.style.color = "red";
-})
+// //另一种制作enent listener 的方法.
+// let btn = document.getElementById("button");
+// btn.addEventListener("click", function () {
 
-document.getElementById("age").addEventListener("focus", function () {
-    let spaceHolder = document.getElementById("age");
-    spaceHolder.value = "23523523525";
-})
+//     btn.style.color = "red";
+// })
 
-document.querySelector("input").addEventListener("blur", function () {
-    document.getElementById("content").innerHTML = "<h1>WHAT FUCK DO YOU WANT, BRUH?</h1>";
-})
+// document.getElementById("age").addEventListener("focus", function () {
+//     let spaceHolder = document.getElementById("age");
+//     spaceHolder.value = "23523523525";
+// })
 
-function changeIt() {
-    let ctt = document.getElementById("content");
-    ctt.innerText = "fuck shit!";
-}
+// document.querySelector("input").addEventListener("blur", function () {
+//     document.getElementById("content").innerHTML = "<h1>WHAT FUCK DO YOU WANT, BRUH?</h1>";
+// })
 
-btn.onclick = changeIt;
+// function changeIt() {
+//     let ctt = document.getElementById("content");
+//     ctt.innerText = "fuck shit!";
+// }
+
+// btn.onclick = changeIt;
 
 // btn.addEventListener("click", changeIt);
+
+// document.querySelector("#box")
+//     .addEventListener("mousemove",
+//         function (event) {
+//             if (event.altKey === true) {
+//                 let ball = document.getElementById("ball");
+//                 ball.style.position = "absolute";
+//                 let delay = 250;
+//                 setTimeout(function () {
+//                     ball.style.top = event.clientY + "px";
+//                     ball.style.left = event.clientX + "px";
+//                 }, delay)
+//                 ball.style.backgroundColor = "red";
+
+//                 console.log("x: " + event.clientX);
+//                 console.log("y: " + event.clientY);
+//             }
+//         }
+//     );
+// // document.getElementById("mario").addEventListener("keydown",
+//     function (event) {
+
+//         if (KeyboardEvent.keyCode === 13) {
+//             mario.style.right = bor.style.left + 10;
+//             console.log("wocao")
+//         }
+//     })
+let ball = document.getElementById("ball");
+let ball2 = document.getElementById("ball2");
+ball2.style.left = 1548 + "px";
+ball2.style.top = 428 + "px";
+
+let bro = document.getElementById("mario");
+ball.style.left = 857 + "px";
+ball.style.top = 149 + "px";
+let score = document.getElementById("score");
+let guai = document.getElementById("guai");
+guai.style.left = 248 + "px";
+guai.style.top = 605 + "px";
+
+
+let input = document.getElementById("input");
+input.addEventListener("keydown",
+    function (event) {
+        if (event.key === " ") {
+
+            let position = parseInt(bro.style.left) || 0;
+            if (position >= 60 && position <= 210) {
+                setTimeout(function () {
+                    bro.style.top = 350 + "px";
+                }, 0);
+                setTimeout(function () {
+                    bro.style.top = 420 + "px";
+                }, 500);
+
+            } else if (position >= 800 && position <= 840) {
+                setTimeout(function () {
+                    bro.style.top = 220 + "px";
+                }, 0);
+                setTimeout(function () {
+                    bro.style.top = 350 + "px";
+                }, 500);
+                setTimeout(function () {
+                    ball.style.top = 50 + "px";
+                }, 100);
+                setTimeout(function () {
+                    ball.style.top = 149 + "px";
+                    score.innerHTML = parseInt(score.innerText) + 1;
+                }, 700);
+
+
+            } else if (position >= 1500 && position <= 1550) {
+                setTimeout(function () {
+                    bro.style.top = 490 + "px";
+                }, 0);
+                setTimeout(function () {
+                    bro.style.top = 559 + "px";
+                }, 500);
+                setTimeout(function () {
+                    ball2.style.top = 350 + "px";
+                }, 100);
+                setTimeout(function () {
+                    ball2.style.top = 428 + "px";
+                    score.innerHTML = parseInt(score.innerText) + 1;
+                }, 700);
+
+            } else if (position >= 770 && position <= 910) {
+                setTimeout(function () {
+                    bro.style.top = 220 + "px";
+                }, 0);
+                setTimeout(function () {
+                    bro.style.top = 350 + "px";
+                }, 500);
+                console.log(position);
+            } else {
+                setTimeout(function () {
+                    bro.style.top = 350 + "px";
+                }, 0);
+                setTimeout(function () {
+                    bro.style.top = 559 + "px";
+                }, 500);
+
+            }
+
+        };
+        if (event.key === "d") {
+            bro.style.backgroundImage = "url(img/marrio2.png)";
+            let current = parseInt(bro.style.left) || 0;
+            let newlf = current + 10;
+            bro.style.left = newlf + "px";
+
+
+        };
+        if (event.key === "a") {
+            bro.style.backgroundImage = "url(img/marrio3.png)";
+            let current = parseInt(bro.style.left) || 0;
+            let newlf = current - 10;
+            bro.style.left = newlf + "px";
+
+        }
+    }
+);
+
+
+input.addEventListener("focus",
+    function () {
+        // while (true) {
+        setTimeout(function () {
+            guai.src = "img/guai.png"
+            guai.style.left = 310 + "px";
+        }, 500)
+        setTimeout(function () {
+            guai.style.left = 350 + "px";
+        }, 1000)
+        setTimeout(function () {
+            guai.style.left = 410 + "px";
+        }, 1500)
+        setTimeout(function () {
+            guai.style.left = 450 + "px";
+        }, 2000)
+        setTimeout(function () {
+            guai.style.left = 510 + "px";
+        }, 2500)
+        setTimeout(function () {
+            guai.style.left = 550 + "px";
+        }, 3000)
+        setTimeout(function () {
+            guai.style.left = 610 + "px";
+        }, 3500)
+        setTimeout(function () {
+            guai.style.left = 650 + "px";
+        }, 4000)
+        setTimeout(function () {
+            guai.style.left = 710 + "px";
+        }, 4500)
+
+        setTimeout(function () {
+            guai.src = "img/guai2.png"
+            guai.style.left = 710 + "px";
+        }, 4500)
+        setTimeout(function () {
+             guai.style.left = 650 + "px";
+        }, 5000)
+        setTimeout(function () {
+            guai.style.left = 600 + "px";
+        }, 5500)
+        setTimeout(function () {
+            guai.style.left = 550 + "px";
+        }, 6000)
+        setTimeout(function () {
+            guai.style.left = 500 + "px";
+        }, 6500)
+        setTimeout(function () {
+            guai.style.left = 450 + "px";
+        }, 7000)
+        setTimeout(function () {
+            guai.style.left = 400 + "px";
+        }, 7500)
+        setTimeout(function () {
+            guai.style.left = 350 + "px";
+        }, 8000)
+        setTimeout(function () {
+            guai.style.left = 310 + "px";
+        }, 8500)
+        setTimeout(function () {
+            guai.style.left = 248 + "px";
+        }, 8500)
+        // }
+    });

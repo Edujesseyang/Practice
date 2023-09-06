95f32539d26afb5f4daca4d75278e0b9c75009dd
Edujesseyang <edujesseyang@gmail.com>
2023-09-05 19:29:48 -0700

    init express js

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const http = require('http');
- 
-const hostname = '127.0.0.1';
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+const express = require('express');
+const app = express();
 const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', (req, res) => {
+  res.send("this is it");
 });
 
-
+app.listen(port, () => {
+  console.log('This app listening on port ' + port);
+})

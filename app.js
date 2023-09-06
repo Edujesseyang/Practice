7c342a152313dae902209cca0d65cc8cdd7383ae
Edujesseyang <edujesseyang@gmail.com>
2023-09-05 20:20:41 -0700

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send("this is it");
+  res.send("this is my shit");
 });
 
 app.listen(port, () => {
